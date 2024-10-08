92e810bac2634ce0ff1fe750b54574568ca92491
cclemon40 <ggwp6900899@gmail.com>
2024-10-08 17:12:57 +0800

    第一版

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -36,7 +36,11 @@ namespace WpfApp2
             }
             else
             {
-                
+                var targetStackPanel = targetTextBox.Parent as StackPanel;
+                var targetLabel = targetStackPanel.Children[0] as Label;
+                var drinkName = targetLabel.Content.ToString();
+                //MessageBox.Show($"您選擇了{drinkName}，數量為{amount}杯", "訂購成功");
+                ResultTextBlock.Text += $"您選擇了{drinkName}，數量為{amount}杯\n";
             }
 
         }
